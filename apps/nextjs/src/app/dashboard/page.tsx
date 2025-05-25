ec3e5d25d8f4aed3018ac0bafc1f1730cdd543bf
Samarth Chitgopekar <67826352+http-samc@users.noreply.github.com>
2025-05-24 19:18:48 -0500

    Update page.tsx

--- apps/nextjs/src/app/dashboard/page.tsx
+++ apps/nextjs/src/app/dashboard/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import type { Track } from "@spotify/web-api-ts-sdk";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { Track } from "@spotify/web-api-ts-sdk";
 import { useQuery } from "@tanstack/react-query";
 import { motion } from "framer-motion";
 import { useDebounce } from "use-debounce";
@@ -52,16 +52,18 @@ const DashboardPage = () => {
     <>
       <div className="flex flex-col items-center space-y-16 p-8">
         <motion.div layoutId="search" className="mx-auto space-y-1">
-          <Label htmlFor="search">Find a song</Label>
+          <Label htmlFor="search" className="text-xs">
+            Search Spotify
+          </Label>
           <Input
             id="search"
             placeholder="e.g. Sweet Child O' Mine"
-            className="rounded-none sm:w-96"
+            className="rounded-none sm:w-96 lg:w-[500px]"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
           />
         </motion.div>
-        <ul className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <ul className="grid w-full gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {searchResults?.tracks.map((track) => (
             <button
               key={track.id}
@@ -71,6 +73,7 @@ const DashboardPage = () => {
               }}
             >
               <Image
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain
                 src={track.album.images[0]?.url!}
                 width={190}
                 height={180}
@@ -92,7 +95,7 @@ const DashboardPage = () => {
                 className="h-64 w-48 space-y-2 border p-2 hover:scale-105"
               >
                 <img
-                  src={playlist.images[0]?.url!}
+                  src={playlist.images[0]?.url}
                   className="aspect-square overflow-hidden object-cover"
                   alt={playlist.name}
                 />
@@ -109,6 +112,11 @@ const DashboardPage = () => {
                 <div className="h-3 w-32 animate-pulse bg-gray-600 delay-200"></div>
               </li>
             ))}
+          {!isFetchingSearchResults && debouncedQuery && !searchResults && (
+            <div className="col-span-full grid h-96 w-full place-content-center border border-dashed">
+              <p className="text-sm text-muted-foreground">No results found.</p>
+            </div>
+          )}
         </ul>
       </div>
       {selectedTrack && (
